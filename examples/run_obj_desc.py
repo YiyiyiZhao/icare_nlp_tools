3c260392cecb69e812f0420e6871f9bce52f3dfa
Yi Zhao <23037086r@connect.polyu.hk>
2024-05-09 16:15:20 +0800

    add usage

--- examples/run_obj_desc.py
+++
@@ -1,8 +0,0 @@
-import json
-from icare_nlp.object_desc import ObjectDesc
-obj_desc=ObjectDesc()
-with open("./obj_detect_files/59.json", "r") as f:
-    obj_detect=json.load(f)
-print(obj_detect)
-obj_desc_res=obj_desc.form_response(obj_detect)
-print(obj_desc_res)
